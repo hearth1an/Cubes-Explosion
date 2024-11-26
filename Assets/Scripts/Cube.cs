b712ce81a13cdb69e9a4488dbf80bd07034beb67
muerterico <muertericobeats@gmail.com>
2024-11-26 22:19:48 +0500

    update scripts

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,53 +1,95 @@
 using UnityEngine;
 using System;
 
+[RequireComponent(typeof(Rigidbody), typeof(Renderer))]
 public class Cube : MonoBehaviour
 {
-    private float _baseExplosionForce = 10f;
-    private float _baseExplosionRadius = 40f;
-
     private int _currentSplitChance = 100;
     private int _maxSplitChance = 100;
     private int _minSplitChance = 0;
     private int _splitChanceReduce = 2;
-    private bool _isSplitted = false;    
+    private bool _isSplitted = false;
 
-    public float baseExplosionForce { get; private set; }
-    public float baseExplosionRadius { get; private set; }
+    public event Action<Cube> SplitRequested;
 
-    public event Action<Cube> SplitAllowed;
+    public float BaseExplosionForce { get; private set; } = 10f;
+    public float BaseExplosionRadius { get; private set; } = 40f;
 
-    private void Awake()
-    {
-        baseExplosionForce = _baseExplosionForce;
-        baseExplosionRadius = _baseExplosionRadius;        
-    }
+    public int CurrentSplitChance => _currentSplitChance;
 
     private void OnMouseDown()
     {
         if (_isSplitted)
             return;
 
-        if (CanSplit())
+        if (TryReduceSplitChance())
         {
-            SplitAllowed?.Invoke(this);            
+            SplitRequested?.Invoke(this);
             _isSplitted = true;
-            TriggerExplosion();
-
-            Destroy(gameObject);
         }
         else
         {
+            TriggerExplosion();
             Destroy(gameObject);
         }
-    }    
+    }
+
+    public void UpdateSplitChance(int chance)
+    {
+        _currentSplitChance = chance;
+    }
+
+    public void ChangeScale(Vector3 scale)
+    {
+        transform.localScale = scale;
+    }
+
+    public void ChangeColor()
+    {
+        GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+    }
+
+    public Vector3 GetScale()
+    {
+        Vector3 scale = transform.localScale;
+
+        return scale;
+    }
+
+    public Vector3 GetPosition()
+    {
+        Vector3 position = transform.position;
+
+        return position;
+    }
+
+    public void AddExplosion(float force, Vector3 center, float radius)
+    {
+        float upwardsModifier = 0.1f;
+
+        GetComponent<Rigidbody>().AddExplosionForce(force, center, radius, upwardsModifier, ForceMode.Impulse);
+    }
+
+    private bool TryReduceSplitChance()
+    {
+        int randomValue = UnityEngine.Random.Range(_minSplitChance, _maxSplitChance);
+
+        if (randomValue <= _currentSplitChance)
+        {
+            _currentSplitChance /= _splitChanceReduce;
+
+            return true;
+        }
+
+        return false;
+    }
 
     private void TriggerExplosion()
     {
         int forceMultiplier = 3;
         float cubeSize = transform.localScale.magnitude;
-        float force = _baseExplosionForce / cubeSize * forceMultiplier;
-        float radius = _baseExplosionRadius / cubeSize * forceMultiplier;
+        float force = BaseExplosionForce / cubeSize * forceMultiplier;
+        float radius = BaseExplosionRadius / cubeSize * forceMultiplier;
         Vector3 center = transform.position;
 
         Collider[] colliders = Physics.OverlapSphere(center, radius);
@@ -64,62 +106,5 @@ public class Cube : MonoBehaviour
                 }
             }
         }
-    }
-
-    public void UpdateSplitChance(int chance)
-    {
-        _currentSplitChance = chance;
-    }
-
-    public int GetCurrentSplitChance()
-    {
-        int chance = _currentSplitChance;
-
-        return chance;
-    }
-
-    public void ChangeScale(Vector3 scale)
-    {
-        gameObject.transform.localScale = scale;
-    }    
-
-    public void ChangeColor()
-    {
-        gameObject.GetComponent<Renderer>().material.color = new Color (UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
-    }
-
-    public Vector3 GetScale()
-    {
-        Vector3 scale = gameObject.transform.localScale;
-
-        return scale;
-    }
-
-    public Vector3 GetPosition()
-    {
-        Vector3 position = gameObject.transform.position;
-
-        return position;
-    }
-
-    public void AddExplosion(float force, Vector3 center, float radius)
-    {
-        float upwardsModifier = 0.1f;
-
-        gameObject.GetComponent<Rigidbody>().AddExplosionForce(force, center, radius, upwardsModifier, ForceMode.Impulse);
-    }
-
-    private bool CanSplit()
-    {
-        int randomValue = UnityEngine.Random.Range(_minSplitChance, _maxSplitChance);
-
-        if (randomValue <= _currentSplitChance)
-        {
-            _currentSplitChance /= _splitChanceReduce;
-
-            return true;
-        }
-
-        return false;
     }
 }
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -4,50 +4,73 @@ using UnityEngine;
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private List<Cube> _cubes;
     [SerializeField] private int _minCubes = 2;
     [SerializeField] private int _maxCubes = 6;
 
-    private void Awake()
+    private List<Cube> _activeCubes = new();
+
+    private void Start()
     {
-        foreach (Cube cube in FindObjectsOfType<Cube>())
+        foreach (Cube cube in _cubes)
         {
-            SubscribeToCube(cube);
-        }        
+            RegisterCube(cube);
+        }
     }
 
-    private void SubscribeToCube(Cube cube)
-    {
-        cube.SplitAllowed += SpawnCubes;
+    private void OnDestroy()
+    {        
+        foreach (Cube cube in _activeCubes)
+        {
+            UnregisterCube(cube);
+        }
     }
 
     public void SpawnCubes(Cube sourceCube)
     {
         int childCount = UnityEngine.Random.Range(_minCubes, _maxCubes + 1);
-        List<Cube> newCubes = new List<Cube>();
+        List<Cube> newCubes = new();
 
         for (int i = 0; i < childCount; i++)
         {
-            newCubes.Add(CreateCube(sourceCube));
+            Cube newCube = CreateCube(sourceCube);
+            newCubes.Add(newCube);
         }
 
         foreach (Cube cube in newCubes)
         {
-            cube.AddExplosion(sourceCube.baseExplosionForce, sourceCube.GetPosition(), sourceCube.baseExplosionRadius);
+            cube.AddExplosion(sourceCube.BaseExplosionForce, sourceCube.GetPosition(), sourceCube.BaseExplosionRadius);
         }
+
+        UnregisterCube(sourceCube);
+        Destroy(sourceCube.gameObject);
     }
+
+    private void RegisterCube(Cube cube)
+    {
+        cube.SplitRequested += SpawnCubes;
+        _activeCubes.Add(cube);
+    }
+
+    private void UnregisterCube(Cube cube)
+    {
+        cube.SplitRequested -= SpawnCubes;
+        _activeCubes.Remove(cube);
+    }    
 
     private Cube CreateCube(Cube sourceCube)
     {
         int scaleReduceValue = 2;
         Vector3 newScale = sourceCube.GetScale() / scaleReduceValue;
 
-        Cube newCube = Instantiate(_cubePrefab, sourceCube.GetPosition(), UnityEngine.Random.rotation);       
+        Cube newCube = Instantiate(_cubePrefab, sourceCube.GetPosition(), UnityEngine.Random.rotation);
 
         newCube.ChangeScale(newScale);
         newCube.ChangeColor();
-        newCube.UpdateSplitChance(sourceCube.GetCurrentSplitChance());
+        newCube.UpdateSplitChance(sourceCube.CurrentSplitChance);
         newCube.GetComponent<Rigidbody>().useGravity = true;
 
+        RegisterCube(newCube);
         return newCube;
     }
 }
